70cba122a6acae8477697442981fc61caf9f8661
Serhiy <sgvcool@gmail.com>
2020-01-20 11:28:13 +0200

    initialization for vars

---
+++ firstTS.js
@@ -0,0 +1,8 @@
+function helloWorld(person) {
+    return "Hellow," + person;
+}
+var user = "USER";
+document.body.innerHTML = helloWorld(user);
+var isDone = false;
+var isDone2 = true;
+console.log(isDone2);
---
+++ firstTS.ts
@@ -0,0 +1,11 @@
+function helloWorld(person){
+  return "Hellow," + person;
+}
+
+let user = "USER";
+document.body.innerHTML=helloWorld(user);
+
+let isDone : boolean = false;
+let isDone2 : boolean = true;
+
+console.log(isDone2);
