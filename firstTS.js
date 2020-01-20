9757144957db6cbc4c854c2e55d786dfe3421ffb
Serhiy <sgvcool@gmail.com>
2020-01-20 12:10:43 +0200

    basic main types

--- firstTS.js
+++ firstTS.js
@@ -1,8 +1,38 @@
 function helloWorld(person) {
-    return "Hellow," + person;
+    return "Hellow, " + person;
 }
 var user = "USER";
 document.body.innerHTML = helloWorld(user);
 var isDone = false;
 var isDone2 = true;
-console.log(isDone2);
+var num = 4;
+var octal = 484;
+var hex = 0xf00d;
+var color = 'FFF';
+var color2 = "FFF";
+var color3 = "color " + user;
+var arr = ['1', '2', '4'];
+var arr2 = [1, 2, 5, 6];
+var arr3 = [1, 2, 5, 6];
+var tupleTypeArray;
+tupleTypeArray = ['fgfg', 3];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Black"] = 2] = "Black";
+    Color[Color["White"] = 3] = "White";
+})(Color || (Color = {}));
+;
+var c = Color.Red;
+var color0 = Color[1];
+document.body.innerHTML += ' ' + color0;
+var m = '4';
+m = 4;
+m = ['3'];
+function showMessage(message) {
+    document.body.innerHTML = message;
+}
+showMessage('bla bla bla shown');
+function isShown() {
+    return true;
+}
--- firstTS.ts
+++ firstTS.ts
@@ -1,5 +1,5 @@
 function helloWorld(person){
-  return "Hellow," + person;
+  return "Hellow, " + person;
 }
 
 let user = "USER";
@@ -8,4 +8,37 @@ document.body.innerHTML=helloWorld(user);
 let isDone : boolean = false;
 let isDone2 : boolean = true;
 
-console.log(isDone2);
+let num : number = 4;
+let octal : number = 0o744;
+let hex : number = 0xf00d;
+
+let color : string = 'FFF';
+let color2 : string = "FFF";
+let color3 : string = `color ${user}`;
+
+let arr : string[] = ['1','2','4'];
+let arr2 : number[] = [1,2,5,6];
+let arr3 : Array<number> = [1,2,5,6];
+
+let tupleTypeArray : [string, number];
+tupleTypeArray  = ['fgfg', 3];
+
+enum Color{Red = 1, Black = 2, White = 3};
+let c: Color = Color.Red;
+let color0 : string = Color[1];
+
+document.body.innerHTML += ' '+ color0;
+
+let m : any = '4';
+m = 4;
+m = ['3'];
+
+function showMessage(message : string) : void{
+  document.body.innerHTML = message;
+}
+
+showMessage('bla bla bla have shown!');
+
+function isShown() : boolean{
+  return true;
+}
