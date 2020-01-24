7220b2372a76f529261396555879883d9e300b75
Serhiy <sgvcool@gmail.com>
2020-01-24 19:55:53 +0200

    'functions'

---
+++ function.js
@@ -0,0 +1,14 @@
+/*function add(a, b){
+    return a+b;
+}*/
+/*let sum = function add(a, b){
+    return a+b;
+};*/
+//sum(3,6);
+//document.write(sum(3,6));
+function add(a, b) {
+    return a + b;
+}
+var sum = add(4, 6);
+document.write(sum.toString());
+document.write('<hr>');
---
+++ function.ts
@@ -0,0 +1,18 @@
+/*function add(a, b){
+    return a+b;
+}*/
+
+/*let sum = function add(a, b){
+    return a+b;
+};*/
+//sum(3,6);
+//document.write(sum(3,6));
+
+
+function add(a:number, b:number):number{
+    return a+b;
+}
+
+let sum = add(4,6);
+document.write(sum.toString());
+document.write('<hr>');
