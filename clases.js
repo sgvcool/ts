ef2b5dd869d69840128ec28b1ba97db5fc98a509
Serhiy <sgvcool@gmail.com>
2020-01-24 09:55:56 +0200

    'add_clases_private_public_protected'

--- clases.js
+++ clases.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 document.write('<hr>');
 var User = /** @class */ (function () {
     function User(userName, password) {
@@ -7,15 +20,19 @@ var User = /** @class */ (function () {
     User.prototype.getUserName = function () {
         return this.userName;
     };
+    User.prototype.getPassword = function () {
+        return this.password;
+    };
     User.prototype.setUserName = function (userName) {
         this.userName = userName;
     };
     User.prototype.sayHelloUser = function () {
-        console.log("Hello " + this.userName + " User Password: " + this.password);
+        return "Hello " + this.userName + " User Password: " + this.password;
     };
     return User;
 }());
-var user = new User('John', 6677);
+/*
+let user = new User('John',6677);
 document.write('<hr>');
 user.getUserName();
 document.write(user.getUserName());
@@ -23,4 +40,29 @@ document.write('<hr>');
 user.setUserName('David');
 document.write(user.getUserName());
 document.write('<hr>');
-user.sayHelloUser();
+document.write(user.sayHelloUser());
+document.write('<hr>');
+*/
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin(userName, password, role) {
+        var _this = _super.call(this, userName, password) || this;
+        /* this.userName = userName;
+         this.password = password;*/
+        _this.role = role;
+        return _this;
+    }
+    Admin.prototype.sayHelloAdmin = function () {
+        return "Hi user: " + this.userName + ". I am a " + this.role;
+    };
+    return Admin;
+}(User));
+var admin = new Admin('Bonjovi', 324, 'Super Admin');
+/*
+document.write('<hr>');
+document.write(admin.sayHelloAdmin());
+document.write('<hr>');*/
+var user = new User('John', 6677);
+document.write('<hr>');
+document.write(user.getPassword().toString());
+document.write('<hr>');
--- clases.ts
+++ clases.ts
@@ -1,8 +1,8 @@
 document.write('<hr>');
 
 class User{
-    userName: string;
-    password: number;
+    public userName: string;
+    private password: number;
 
     constructor(userName:string, password:number){
         this.userName = userName;
@@ -13,15 +13,20 @@ class User{
         return this.userName;
     }
 
+    public getPassword(){
+        return this.password;
+    }
+
     setUserName(userName:string){
         this.userName = userName;
     }
 
     sayHelloUser(){
-        console.log(`Hello ${this.userName} User Password: ${this.password}`);
+        return `Hello ${this.userName} User Password: ${this.password}`;
     }
 }
 
+/*
 let user = new User('John',6677);
 document.write('<hr>');
 user.getUserName();
@@ -30,4 +35,38 @@ document.write('<hr>');
 user.setUserName('David');
 document.write(user.getUserName());
 document.write('<hr>');
-user.sayHelloUser();
+document.write(user.sayHelloUser());
+document.write('<hr>');
+*/
+
+class Admin extends User{
+
+    role: string;
+    constructor(userName:string, password:number, role: string){
+        super(userName,password);
+
+       /* this.userName = userName;
+        this.password = password;*/
+        this.role = role;
+    }
+
+    sayHelloAdmin(){
+        return `Hi user: ${this.userName}. I am a ${this.role}`;
+    }
+}
+
+let admin = new Admin('Bonjovi',324,'Super Admin');
+
+/*
+document.write('<hr>');
+document.write(admin.sayHelloAdmin());
+document.write('<hr>');*/
+
+
+let user = new User('John',6677);
+
+document.write('<hr>');
+document.write(user.getPassword().toString());
+document.write('<hr>');
+
+
