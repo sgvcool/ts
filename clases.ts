8393fa7373df910e3e9b0c7cc330dab435790900
Serhiy <sgvcool@gmail.com>
2020-01-24 19:20:07 +0200

    'getter_and_setter_in_class'

--- clases.ts
+++ clases.ts
@@ -2,80 +2,26 @@ document.write('<hr>');
 
 class User{
     public userName: string;
-    private password: number;
+    public password: number;
     readonly createDate: object;
 
-    constructor(userName:string, password:number){
+    constructor(userName:string){
         this.userName = userName;
-        this.password = password;
         this.createDate = new Date();
-
     }
 
-    getUserName(){
-        return this.userName;
-    }
-
-    public getPassword(){
+    get password(){
         return this.password;
     }
 
-    setUserName(userName:string){
-        this.userName = userName;
-    }
-
-    sayHelloUser(){
-        return `Hello ${this.userName} User Password: ${this.password}`;
+    set password(pass:number){
+        this.password = pass;
     }
 }
 
-/*
-let user = new User('John',6677);
-document.write('<hr>');
-user.getUserName();
-document.write(user.getUserName());
-document.write('<hr>');
-user.setUserName('David');
-document.write(user.getUserName());
-document.write('<hr>');
-document.write(user.sayHelloUser());
-document.write('<hr>');
-*/
-
-class Admin extends User{
-
-    role: string;
-    constructor(userName:string, password:number, role: string){
-        super(userName,password);
-
-       /* this.userName = userName;
-        this.password = password;*/
-        this.role = role;
-    }
-
-    sayHelloAdmin(){
-        return `Hi user: ${this.userName}. I am a ${this.role}`;
-    }
-}
-
-let admin = new Admin('Bonjovi',324,'Super Admin');
-
-/*
-document.write('<hr>');
-document.write(admin.sayHelloAdmin());
-document.write('<hr>');*/
-
-
-let user = new User('John',6677);
+let user = new User('John');
+user.password = 122345;
 
 document.write('<hr>');
-document.write(user.getPassword().toString());
+document.write(user.password.toString());
 document.write('<hr>');
-
-
-
-document.write(user.createDate.toString());
-
-document.write('<hr>');
-//user.createDate = {'key':1}; read only error
-
